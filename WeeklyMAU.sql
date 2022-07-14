d1f9711bcaede6d7d9a25ad374de15062237c47a
Zane <zane.sand@photomath.com>
2022-07-14 10:45:36 -0700

    Change WeeklyMAU.sql

--- WeeklyMAU.sql
+++ WeeklyMAU.sql
@@ -1,9 +1,9 @@
 /*
 Monthly Active Users by Week
 
-Date | Market | ActualMAU | TargetMAU
--------------------------------------
-...  | ...    | ...       | ...
+Date (every Sunday) | Market | Actual | Target
+----------------------------------------------
+...                 | ...    | ...    | ...
 
 Dashboard: https://datastudio.google.com/reporting/b0ea098f-343e-4e50-8e3d-6dee640c0167
 */
@@ -12,64 +12,36 @@ CREATE OR REPLACE TABLE `erudite-idea-777.Zane_BTSDashboard.WeeklyMAU` AS
 
 WITH
 
-historicalDAU AS (
-  SELECT Date AS EventDate,
-         c.Region AS Market,
-         UserID
-  FROM `erudite-idea-777.analytics_151921982.historical_dau` h
-  JOIN `erudite-idea-777.Zane_BTSDashboard.CountryRegion` c
-    ON h.country = c.Country
-  WHERE CountUserEngagement > 0
-    AND Date >= '2020-12-01'
-),
-
-dates AS (
-  SELECT Date
-  FROM UNNEST(
-    GENERATE_DATE_ARRAY(DATE('2021-01-01'), DATE('2022-12-31'), INTERVAL 7 DAY)
-  ) Date
-),
-
-eventsWithDate AS (
-  SELECT dates.Date AS EventDate,
-         historicalDAU.Market,
-         historicalDAU.UserID
-  FROM historicalDAU
-  JOIN dates
-    ON DATE_DIFF(dates.Date, historicalDAU.EventDate, DAY) <= 30
-   AND DATE_DIFF(dates.Date, historicalDAU.EventDate, DAY) >= 0
+daily_MAU AS (
+  SELECT mau_tbl.DATE,
+         CASE WHEN cr.Region IS NULL THEN 'Rest of World' ELSE cr.Region END AS Region,
+         SUM(mau_tbl.MAU) AS MAU
+  FROM `photomath-dwh-prod.DWH.TBL_M_APP_MAU` AS mau_tbl
+  LEFT JOIN `erudite-idea-777.Zane_BTSDashboard.CountryRegion` AS cr
+    ON mau_tbl.COUNTRY_NAME = cr.Country
+  GROUP BY mau_tbl.DATE, cr.Region
 ),
 
 MAU2022 AS (
-  SELECT EventDate,
-         Market,
-         COUNT(DISTINCT UserID) AS ActualMAU
-  FROM eventsWithDate
-  WHERE EXTRACT(YEAR FROM eventsWithDate.EventDate) = 2022
-  GROUP BY EventDate, Market
-),
-
-MAU2021 AS (
-  SELECT EventDate,
-         Market,
-         COUNT(DISTINCT UserID) AS ActualMAU
-  FROM eventsWithDate
-  WHERE EXTRACT(YEAR FROM eventsWithDate.EventDate) = 2021
-  GROUP BY EventDate, Market
+  SELECT *
+  FROM daily_MAU
+  WHERE EXTRACT(YEAR FROM DATE) = 2022
 ),
 
 TargetMAU2022 AS (
-  SELECT EventDate,
-         Market,
-         ActualMAU * 1.4 AS TargetMAU
-  FROM MAU2021
+  SELECT DATE_ADD(DATE, INTERVAL 1 YEAR) AS DATE,
+         Region,
+         MAU * 1.4 AS TargetMAU
+  FROM daily_MAU
+  WHERE EXTRACT(YEAR FROM DATE) = 2021
 )
 
-SELECT DATE_ADD(TargetMAU2022.EventDate, INTERVAL 1 YEAR) AS Date,
-       TargetMAU2022.Market,
-       MAU2022.ActualMAU,
-       TargetMAU2022.TargetMAU
+SELECT TargetMAU2022.DATE AS Date,
+       TargetMAU2022.Region AS Market,
+       MAU2022.MAU AS Actual,
+       TargetMAU2022.TargetMAU AS Target
 FROM TargetMAU2022
 LEFT JOIN MAU2022
-  ON EXTRACT(WEEK FROM MAU2022.EventDate) = EXTRACT(WEEK FROM TargetMAU2022.EventDate)
- AND MAU2022.Market = TargetMAU2022.Market
+  ON TargetMAU2022.DATE = MAU2022.DATE
+ AND TargetMAU2022.Region = MAU2022.Region
+WHERE EXTRACT(DAYOFWEEK FROM TargetMAU2022.DATE) = 1
