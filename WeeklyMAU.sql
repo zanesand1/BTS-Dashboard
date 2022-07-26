3c7eeaea9d32b3d9416b31606f5b9f9bcb2586a8
Zane <zane.sand@photomath.com>
2022-07-26 11:03:07 -0700

    Update WeeklyMAU.sql

--- WeeklyMAU.sql
+++ WeeklyMAU.sql
@@ -1,10 +1,8 @@
 /*
 Monthly Active Users by Week
-
 Date (every Sunday) | Market | Actual | Target
 ----------------------------------------------
 ...                 | ...    | ...    | ...
-
 Dashboard: https://datastudio.google.com/reporting/b0ea098f-343e-4e50-8e3d-6dee640c0167
 */
 
@@ -12,14 +10,37 @@ CREATE OR REPLACE TABLE `erudite-idea-777.Zane_BTSDashboard.WeeklyMAU` AS
 
 WITH
 
+historicalDAU AS (
+  SELECT h.Date,
+         CASE WHEN c.CountryID IS NULL THEN 'Rest of World' ELSE c.Region END AS Region,
+         h.UserID
+  FROM `erudite-idea-777.analytics_151921982.historical_dau` h
+  LEFT JOIN `photomath-dwh-prod.DWH.TBL_D_DEVICE` d
+    ON h.UserID = d.DEVICE_ID
+  LEFT JOIN `erudite-idea-777.Zane_BTSDashboard.CountryRegion` c
+    ON d.COUNTRY_ID = c.CountryID
+  WHERE CountUserEngagement > 0
+    AND EXTRACT(YEAR FROM Date) >= 2021
+),
+
+dates AS (
+  SELECT date
+  FROM UNNEST(
+    GENERATE_DATE_ARRAY(
+      DATE('2021-01-01'), DATE('2022-12-31'), INTERVAL 1 DAY
+    ) 
+  ) date
+),
+
 daily_MAU AS (
-  SELECT mau_tbl.DATE,
-         CASE WHEN cr.Region IS NULL THEN 'Rest of World' ELSE cr.Region END AS Region,
-         SUM(mau_tbl.MAU) AS MAU
-  FROM `photomath-dwh-prod.DWH.TBL_M_APP_MAU` AS mau_tbl
-  LEFT JOIN `erudite-idea-777.Zane_BTSDashboard.CountryRegion` AS cr
-    ON mau_tbl.COUNTRY_NAME = cr.Country
-  GROUP BY mau_tbl.DATE, cr.Region
+  SELECT dates.Date AS DATE,
+        historicalDAU.Region,
+        COUNT(DISTINCT historicalDAU.UserID) AS MAU
+  FROM historicalDAU
+  JOIN dates
+    ON DATE_DIFF(historicalDAU.Date, dates.Date, DAY) <= 30
+  AND DATE_DIFF(historicalDAU.Date, dates.Date, DAY) >= 0
+  GROUP BY dates.Date, historicalDAU.Region
 ),
 
 MAU2022 AS (
@@ -44,4 +65,4 @@ FROM TargetMAU2022
 LEFT JOIN MAU2022
   ON TargetMAU2022.DATE = MAU2022.DATE
  AND TargetMAU2022.Region = MAU2022.Region
-WHERE EXTRACT(DAYOFWEEK FROM TargetMAU2022.DATE) = 1
+WHERE EXTRACT(DAYOFWEEK FROM TargetMAU2022.DATE) = 1;
